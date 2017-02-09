3bcf3efff0efd0572efdb84ef84f23a09abf3beb
github username <github email address>
2017-02-09 13:53:20 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+   attr_accessor :word,:matched
+   def initialize(word)
+     @word = word
+     @matched = [ ]
+   end
+    def match(matching_word)
+      matching_word.each do |ele|
+
+       @matched << ele if @word.split('').sort.join  == ele.split('').sort.join
+     end
+     @matched
+
+
+    end
+end
